c0c4616e833a8fb52fa0e98b7b10b3d9a3a9d421
Sanadhi2002 <sanadhi2002@gmail.com>
2023-05-19 18:24:32 +0530

    changes

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -13,7 +13,7 @@
         class="flex items-center justify-between flex-wrap bg-black py-4 lg:px-12 shadow border-solid border-t-2 border-black-700">
         <div class="flex justify-between lg:w-auto w-full lg:border-b-0 pl-6 pr-2 border-solid border-b-2 border-gray-300 pb-5 lg:pb-0">
             <div class="flex items-center flex-shrink-0 text-gray-800 mr-16">
-                <span class="font-semibold text-xl tracking-tight text-white">POINT</span>
+              <img src="images\light.jpg" class=" h-10 w-18"></img>
             </div>
             <div class="block lg:hidden ">
                 <button
